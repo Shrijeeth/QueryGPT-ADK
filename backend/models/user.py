6a32978e028439556f94bdbd0f22a95a9c236398
Shrijeeth-Suresh <shrijeeth.s@shopup.org>
2025-05-20 02:08:25 +0530

    refactor: remove LLMCredential model and related migrations

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import relationship
 
 from .base import Base
 
@@ -12,5 +11,3 @@ class User(Base):
     email = Column(String(255), unique=True)
     hashed_password = Column(String(255), nullable=False)
     disabled = Column(Boolean, default=False)
-
-    llm_credentials = relationship("LLMCredential", back_populates="user")
